cde3ef9937a6d499e80908133561942b0e62e0a4
daletavg <daletavg@gmail.com>
2019-01-22 15:50:35 +0200

    add messages

---
+++ HW1_ASP/Messages.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Messages.aspx.cs" Inherits="HW1_ASP.Messages" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            
+
+            <asp:TextBox ID="messsage_text" runat="server" Width="30em"></asp:TextBox>
+            <asp:Button ID="message_send" runat="server" Text="Button" />
+            <br/>
+            <asp:PlaceHolder ID="msgs" runat="server"></asp:PlaceHolder>
+        </div>
+    </form>
+</body>
+</html>
---
+++ HW1_ASP/Messages.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace HW1_ASP
+{
+    public partial class Messages : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Label lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+             lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+             lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+             lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+             lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+             lbl = new Label();
+            lbl.Text = "Al<br/>10.0.0.220<br/>22.01.2019<br/>ffejfejfioejfe";
+            msgs.Controls.Add(lbl);
+        }
+    }
+}
